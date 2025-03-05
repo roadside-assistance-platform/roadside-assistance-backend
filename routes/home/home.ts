7a6c93996e4a60e2d9ae771593d7a6592f7abd28
Lachi-Amine <145430686+Lachi-Amine@users.noreply.github.com>
2025-03-05 15:47:15 +0100

    login logger

--- routes/home/home.ts
+++ routes/home/home.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
   if (req.isAuthenticated()) {
-    res.send("Success! You are logged in.");
+    res.status(200).send("Success! You are logged in.");
   } else {
     res.redirect("/login");
   }
