9ee25ce99307d047ee1d07e61b31603932c734a4
Lachi-Amine <145430686+Lachi-Amine@users.noreply.github.com>
2025-03-10 01:05:49 +0100

    logger update
    logger update

--- routes/client/signup.ts
+++ routes/client/signup.ts
@@ -1,6 +1,7 @@
 import prisma from "../../app";
 import { hashPassword } from "../../utilities/bcrypt";
 import { Router } from "express";
+import logger from "../../utilities/logger";
 const router = Router();
 
 const createUser = async (role: string, data: any) => {
@@ -15,6 +16,7 @@ const createUser = async (role: string, data: any) => {
 router.post("/", async (req: any, res: any) => {
   const { email, password, fullName, phone, photo } = req.body;
   if (!email || !password || !fullName || !phone || !photo) {
+    logger.error("All fields are required: email, password, fullName, phone, photo");
     return res.status(400).send("All fields are required: email, password, fullName, phone, photo");
   }
 
@@ -25,15 +27,16 @@ router.post("/", async (req: any, res: any) => {
       },
     });
     if (existingUser) {
+      logger.error("Client with this email or phone already exists");
       return res.status(400).send("Client with this email or phone already exists");
     }
 
     const hashedPassword = await hashPassword(password);
     const newUser = await createUser("client", { email, password: hashedPassword, fullName, phone, photo });
-
+logger.info(`New client created: ${email}`);
     res.status(201).json(newUser);
   } catch (error) {
-    console.error(error);
+    logger.error(error);
     res.status(500).send("An error occurred while creating the client");
   }
 });
--- routes/provider/googleAuth.ts
+++ routes/provider/googleAuth.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import passport from "../../utilities/passport";
+import logger from "../../utilities/logger";
 const router = Router();
 
 router.get(
@@ -11,11 +12,13 @@ router.get(
   "/auth/google/provider/callback",
   passport.authenticate("google-provider", { failureRedirect: "/auth/google/provider/failure" }),
   (req, res) => {
+    logger.info("Google authentication successful for provider");
     res.json({ message: "Google authentication successful", user: req.user });
   }
 );
 
 router.get("/auth/google/provider/failure", (req, res) => {
+  logger.error("Google authentication failed for provider");
   res.status(401).json({ message: "Google authentication failed for provider" });
 });
 
