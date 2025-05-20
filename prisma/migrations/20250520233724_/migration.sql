96aec708134c4f8dd753e9bb81ddef37512b6bcb
Lachi-Amine <145430686+Lachi-Amine@users.noreply.github.com>
2025-05-21 00:48:33 +0100

    dd

---
+++ prisma/migrations/20250520233724_/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "VerificationCode" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "used" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VerificationCode_pkey" PRIMARY KEY ("id")
+);
